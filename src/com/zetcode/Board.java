3445c1bfc89ffe6ef826c4cdef0463eafd0f72f2
ankur2710 <ankurghosh271005@gmail.com>
2026-05-06 23:17:34 +0530

    Add key event handling for board movement

--- src/com/zetcode/Board.java
+++ src/com/zetcode/Board.java
@@ -209,3 +209,37 @@ public class Board extends JPanel implements ActionListener {
 
         repaint();
     }
+
+private class TAdapter extends KeyAdapter {
+
+        @Override
+        public void keyPressed(KeyEvent e) {
+
+            int key = e.getKeyCode();
+
+            if ((key == KeyEvent.VK_LEFT) && (!rightDirection)) {
+                leftDirection = true;
+                upDirection = false;
+                downDirection = false;
+            }
+
+            if ((key == KeyEvent.VK_RIGHT) && (!leftDirection)) {
+                rightDirection = true;
+                upDirection = false;
+                downDirection = false;
+            }
+
+            if ((key == KeyEvent.VK_UP) && (!downDirection)) {
+                upDirection = true;
+                rightDirection = false;
+                leftDirection = false;
+            }
+
+            if ((key == KeyEvent.VK_DOWN) && (!upDirection)) {
+                downDirection = true;
+                rightDirection = false;
+                leftDirection = false;
+            }
+        }
+    }
+}
