1fbde52fbb7fe4a8b06117816ddb5062d47aa8c7
ankur2710 <ankurghosh271005@gmail.com>
2026-05-07 23:20:45 +0530

    Set up Snake game UI in Snake class
    Initialize the Snake game UI with a Board component.

--- src/com/zetcode/Snake.java
+++ src/com/zetcode/Snake.java
@@ -1,1 +1,23 @@
+package com.zetcode;
 
+import java.awt.EventQueue;
+import javax.swing.JFrame;
+
+public class Snake extends JFrame {
+
+    public Snake() {
+        
+        initUI();
+    }
+    
+    private void initUI() {
+        
+        add(new Board());
+               
+        setResizable(false);
+        pack();
+        
+        setTitle("Snake");
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
