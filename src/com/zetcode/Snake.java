7c21be36359119e788f94733542a73d8ae127aee
ankur2710 <ankurghosh271005@gmail.com>
2026-05-07 23:22:29 +0530

    Add main method to launch Snake game

--- src/com/zetcode/Snake.java
+++ src/com/zetcode/Snake.java
@@ -21,3 +21,12 @@ public class Snake extends JFrame {
         setLocationRelativeTo(null);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
+
+public static void main(String[] args) {
+        
+        EventQueue.invokeLater(() -> {
+            JFrame ex = new Snake();
+            ex.setVisible(true);
+        });
+    }
+}
