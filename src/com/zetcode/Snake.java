9fe283f414785517bf74e7d985114c70a00dbc47
Ankur Ghosh <ankurghosh271005@gmail.com>
2026-07-20 22:59:17 +0530

    Implement constructor for Snake class
    Add constructor to initialize Snake UI

--- src/com/zetcode/Snake.java
+++ src/com/zetcode/Snake.java
@@ -2,4 +2,12 @@ package com.zetcode;
 
 import java.awt.EventQueue;
 import javax.swing.JFrame;
+ 
+  public class Snake extends JFrame {
 
+    public Snake() {
+        
+        initUI();
+    }
+    
+   
